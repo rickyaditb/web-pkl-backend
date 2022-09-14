5dc47ac67dcc13eeec40cb7a508cf8530bddf1c3
rickyaditb <rickyaditb@gmail.com>
2022-09-14 17:07:22 +0700

    fix: typo in token controller

--- controller/RefreshToken.js
+++ controller/RefreshToken.js
@@ -13,7 +13,7 @@ export const refreshToken = async(req, res) => {
         if(!user) return res.sendStatus(403);
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
             if(err) return res.sendStatus(403);
-            const userId = user[0].id;
+            const userId = user[0]._id;
             const nama = user[0].nama;
             const email = user[0].email;
             const telepon = user[0].telepon;
